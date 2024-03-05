437e1f881859d52ef2f15dbbaa17329c3d57e94f
Lajja123 <lajja.lamprostech@gmail.com>
2024-03-05 17:11:05 +0530

    make responsive for 1024 screen

--- app/navbar.js
+++ app/navbar.js
@@ -1,30 +1,26 @@
 import Image from "next/image";
-import logo from "../public/BeatSwapLogo.png"
-import navStyle from "@/app/navbar.module.css"
+import logo from "../public/BeatSwapLogo.png";
+import navStyle from "@/app/navbar.module.css";
 import Link from "next/link";
 
 export default function Navbar() {
-    return(
-    
-      <div className={navStyle.divtoflexlogoconnectwallet}>
-        <div className={navStyle.logoimg}>
-          <Link href="/">
-            <Image
-              className={navStyle.smartlogportal}
-              src={logo}
-              alt="not foundd"
-            />
-          </Link>
-        </div>
-
-        <div className={navStyle.connectwalletbuttondiv}>
-          <button className={navStyle.navBtn}>
-            LAUNCH APP
-          </button>
-        </div>
-
+  return (
+    <div className={navStyle.divtoflexlogoconnectwallet}>
+      <div className={navStyle.logoimg}>
+        <Link href="/">
+          <Image
+            className={navStyle.smartlogportal}
+            src={logo}
+            alt="not foundd"
+          />
+        </Link>
       </div>
-    
 
-    )
+      <div className={navStyle.connectwalletbuttondiv}>
+        <Link href="https://dex-interface-git-master-darshits-projects-b9e4d3eb.vercel.app/#/add/ETH">
+          <button className={navStyle.navBtn}>LAUNCH APP</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,48 +1,48 @@
 import Image from "next/image";
-import main from "@/app/main.module.css"
-import token from "@/public/token.png"
-import flower from "@/public/flower.png"
+import main from "@/app/main.module.css";
+import token from "@/public/token.png";
+import flower from "@/public/flower.png";
 import Navbar from "./navbar";
-import titleImg from "@/public/Subtract.png"
-import union from "@/public/Union.png"
-import star from "@/public/star.png"
+import titleImg from "@/public/Subtract.png";
+import union from "@/public/Union.png";
+import star from "@/public/star.png";
 
 export default function Home() {
   return (
-    // main class - full screen
     <main className={main.homeMainDiv}>
-      <Navbar/>
+      <Navbar />
       <div className={main.boxContainer}>
-        <Image src={titleImg} alt="Box Image" className={main.imgdiv}/>
+        <Image src={titleImg} alt="Box Image" className={main.imgdiv} />
         <div className={main.textOverlay}>
-          A DEX Where Your Assets Flow As Smoothly As Music 
-          <Image src={star} alt="star" className={main.starimg} style={{position:"absolute"}}/>
+          A DEX Where Your Assets Flow As Smoothly As Music
+          <Image
+            src={star}
+            alt="star"
+            className={main.starimg}
+            style={{ position: "absolute" }}
+          />
         </div>
       </div>
 
       <div className={main.boxContainer2}>
-
         <div className={main.tokenBox}>
-          <Image src={token} alt="logo" className={main.tokenImg}/> 
+          <Image src={token} alt="logo" className={main.tokenImg} />
         </div>
 
         <div className={main.secdiv}>
           <div className={main.redimg}>
-            <Image src={union} alt="Box Image" className={main.imgdiv2}/>
+            <Image src={union} alt="Box Image" className={main.imgdiv2} />
           </div>
           <div className={main.textOverlay2}>
-            Experience Seamless Trading, Instant Liquidity, Unmatched Security, Experience the Future of Decentralized Finance.
-          </div>
-          <div className={main.sec2Img}>
-            <Image src={flower} alt="logo" className={main.flower}/>
+            Experience Seamless Trading, Instant Liquidity, Unmatched Security,
+            Experience the Future of Decentralized Finance.
           </div>
         </div>
-
       </div>
-
-      <div className={main.footer}>
-        Copyright BeatSwap @2024
+      <div className={main.sec2Img}>
+        <Image src={flower} alt="logo" className={main.flower} />
       </div>
+      <div className={main.footer}>Copyright BeatSwap @2024</div>
     </main>
   );
 }
