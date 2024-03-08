211909ac5d95e120fa33236a849c3d68a05d0a25
Lajja123 <lajja.lamprostech@gmail.com>
2024-03-08 10:54:49 +0530

    replace the favicon

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Poppins({ subsets: ["latin"],weight:['400'] });
 
 export const metadata = {
-  title: "BeatSwap",
+  title: "UdonSwap",
   description: "A DEX Where Your Assets Flow As Smoothly As Music",
 };
 
