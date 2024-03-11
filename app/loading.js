32eae3567a3787b8ae95d1f32a3dcc5c6619518b
isha-bagadiya <ishabagadiya@lampros.tech>
2024-03-11 14:32:34 +0530

    add loader.js file

---
+++ app/loading.js
@@ -0,0 +1,17 @@
+import loaderStyle from "./loading.module.scss";
+export default function Loading() {
+  return (
+    <div className={loaderStyle.center}>
+      <div className={loaderStyle.wave}></div>
+      <div className={loaderStyle.wave}></div>
+      <div className={loaderStyle.wave}></div>
+      <div className={loaderStyle.wave}></div>
+      <div className={loaderStyle.wave}></div>
+      <div className={loaderStyle.wave}></div>
+      <div className={loaderStyle.wave}></div>
+      <div className={loaderStyle.wave}></div>
+      <div className={loaderStyle.wave}></div>
+      <div className={loaderStyle.wave}></div>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -25,6 +25,8 @@ export default function Home() {
             style={{ position: "absolute" }}
           />
         </div>
+
+
       </div>
 
       <div className={main.boxContainer2}>
