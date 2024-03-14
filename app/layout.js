0984ee8fe2082447b0986dd8f704dfe5ccd9922f
Lajja123 <lajja.lamprostech@gmail.com>
2024-03-14 18:21:17 +0530

    add metatags img

--- app/layout.js
+++ app/layout.js
@@ -4,19 +4,6 @@ import "./globals.css";
 
 const inter = Poppins({ subsets: ["latin"],weight:['400'] });
 
-export const metadata = {
-  title: "UdonSwap",
-  description: "A DEX Where Your Assets Flow As Smoothly As Music",
-  images: [
-      {
-        url:"https://gateway.lighthouse.storage/ipfs/QmQgtmCNXCgAywPcqofYKWAqU5Tb1YWYKtCYjLuio6uLgT", // Must be an absolute URL
-        width: 800,
-        height: 600,
-      },
-      
-    ],
-};
-
 
 
 export default function RootLayout({ children }) {
--- app/page.js
+++ app/page.js
@@ -10,3 +10,32 @@ export default function Home() {
     </main>
   );
 }
+
+
+export const metadata = {
+  // metadataBase: new URL('https://localhost:3000'),
+  title: "UdonSwap",
+  description: "A DEX Where Your Assets Flow As Smoothly As Music",
+    openGraph: {
+      title: "Home Page",
+      description: "Home Page Description...",
+      url: "https://beatswap-eta.vercel.app/",
+      siteName: "UdonSwap",
+      images: [
+        {
+          url:"https://app.optimism.io/og-image.png", // Must be an absolute URL
+          width: 800,
+          height: 600,
+        },
+        {
+              url: "https://app.optimism.io/og-image.png", // Must be an absolute URL
+              width: 1800,
+              height: 1600,
+              alt: "My custom alt",
+            },
+        
+      ],
+      locale: "en_US",
+      type: "website",
+    },
+};
